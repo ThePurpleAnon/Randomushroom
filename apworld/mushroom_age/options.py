3db87d4c07f1404680308747beb90ceb6dcf96df
ThePurpleAnon <jahnjohnjones@gmail.com>
2026-09-14 22:25:24 -0500

    add options.py and web_world.py
    and a setup guide that's blank as of now

--- apworld/mushroom_age/options.py
+++ apworld/mushroom_age/options.py
@@ -1,3 +1,33 @@
 from dataclasses import dataclass
 
 from Options import Choice, OptionGroup, PerGameCommonOptions, Range, Toggle
+
+
+class TrapChance(Range):
+    """
+    Percentage chance that any given filler item will be replaced by a trap item.
+    """
+
+    display_name = "Trap Chance"
+
+    range_start = 0
+    range_end = 100
+    default = 0
+
+
+@dataclass
+class MushroomAgeOptions(PerGameCommonOptions):
+    trap_chance: TrapChance
+
+option_groups = [
+    OptionGroup(
+        "Gameplay Options",
+        [TrapChance],
+    ),
+]
+
+option_presets = {
+    "annoying": {
+        "trap_chance": 50,
+    },
+}
--- apworld/mushroom_age/web_world.py
+++ apworld/mushroom_age/web_world.py
@@ -2,3 +2,22 @@ from BaseClasses import Tutorial
 from worlds.AutoWorld import WebWorld
 
 from .options import option_groups, option_presets
+
+
+class MushroomAgeWebWorld(WebWorld):
+    game = "Mushroom Age"
+    theme = "jungle"
+
+    setup_en = Tutorial(
+        "Multiworld Setup Guide",
+        "A guide to setting up Mushroom Age for MultiWorld.",
+        "English",
+        "setup_en.md",
+        "setup/en",
+        ["ThePurpleAnon"],
+    )
+
+    tutorials = [setup_en]
+
+    option_groups = option_groups
+    options_presets = option_presets
